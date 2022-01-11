4026a6c14fe2f0ba654bb2d4ebfc925ffe6a2b33
eswaribala <parameswaribala@gmail.com>
2022-01-11 22:39:21 +0530

    updated

--- cucumberdemo/src/test/java/com/amex/cucumberdemo/StepDefinitions.java
+++ cucumberdemo/src/test/java/com/amex/cucumberdemo/StepDefinitions.java
@@ -7,20 +7,48 @@ import lombok.extern.slf4j.Slf4j;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 @Slf4j
 public class StepDefinitions {
 	private WebDriver webDriver;
     @Given("^Open the Chrome and Launch the application$")
 	public void openChrome_LaunchtheApplication() {
 		
-    	 System.setProperty("webdriver.chrome.driver","c:/chromedriver.exe");
-			webDriver=new ChromeDriver();			
+    	String username = System.getenv("BROWSERSTACK_USERNAME");
+    	String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    	String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+    	String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+    	String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+
+    	DesiredCapabilities capabilities = new DesiredCapabilities();
+    	capabilities.setCapability("os", "Windows");
+    	capabilities.setCapability("os_version", "10");
+    	capabilities.setCapability("browser", "chrome");
+    	capabilities.setCapability("browser_version", "latest");
+    	capabilities.setCapability("name", "BStack-[Java] Sample Test"); // test buildName
+    	capabilities.setCapability("build", buildName); // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+    	capabilities.setCapability("browserstack.local", browserstackLocal);
+    	capabilities.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+    	try {
+			webDriver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
 			webDriver.manage().window().maximize();
 			webDriver.get("http://demo.guru99.com/v4");	
-	       log.info("This Step open the Chrome and launch the application.");				
+	       log.info("This Step open the Chrome and launch the application.");	
+    	
+    	} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	// System.setProperty("webdriver.chrome.driver","c:/chromedriver.exe");
+			//webDriver=new ChromeDriver();			
+						
 	    
 	}
     
