45530e8fc38d81df307f281b16987db576640f92
eswaribala <parameswaribala@gmail.com>
2022-01-21 16:02:51 +0530

    updated

---
+++ captchagen/src/main/java/com/veb/captchagen/CaptchagenApplication.java
@@ -0,0 +1,13 @@
+package com.veb.captchagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchagenApplication.class, args);
+	}
+
+}
---
+++ captchagen/src/test/java/com/veb/captchagen/CaptchagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veb.captchagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
